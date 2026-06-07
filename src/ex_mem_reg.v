d6bdddb9fc1e8fd7b5413b34522bfa20617f5832
akulasahasra75 <akulasahasra0705@gmail.com>
2026-06-07 11:24:23 +0530

    add ex_mem_reg and mem_wb_reg, all pipeline registers complete

--- src/ex_mem_reg.v
+++ src/ex_mem_reg.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 07.06.2026 10:54:31
+// Create Date: 07.06.2026 11:11:42
 // Design Name: 
-// Module Name: id_ex_reg
+// Module Name: ex_mem_reg
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,44 +20,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module id_ex_reg(input clk,rst,flush,stall,
-input [31:0] pc_plus4_in,rd1_in,rd2_in,imm_in,
-input [4:0] rs1_in, rs2_in,rd_in,
-input [2:0] alu_op_in,
-input reg_write_in,alu_src_in,mem_write_in, mem_read_in, mem_to_reg_in, branch_in,
-output reg [31:0] pc_plus4_out,rd1_out,rd2_out, imm_out,
-output reg [4:0] rs1_out, rs2_out, rd_out,
-output reg [2:0] alu_op_out,
-output reg reg_write_out, alu_src_out, mem_write_out, mem_read_out, mem_to_reg_out, branch_out);
+module ex_mem_reg(
+input clk, rst, flush, stall,
+input [31:0] alu_result_in, rd2_in, pc_branch_in,
+input [4:0] rd_in,
+input reg_write_in, mem_write_in, mem_read_in, mem_to_reg_in, branch_in, zero_in,
+output reg [31:0] alu_result_out, rd2_out, pc_branch_out,
+output reg [4:0] rd_out,
+output reg reg_write_out, mem_write_out, mem_read_out, mem_to_reg_out, branch_out, zero_out);
 
 always @(posedge clk)begin
     if(rst | flush)begin
-        pc_plus4_out <=0;
-        rd1_out <= 0;
+        alu_result_out <=0;
         rd2_out <= 0;
-        imm_out <= 0;
-        rs1_out <= 0; 
-        rs2_out <= 0; 
         rd_out <= 0; 
-        alu_op_out <= 0;
+        pc_branch_out <= 0;
         reg_write_out <= 0;
-        alu_src_out <= 0;
+        zero_out <= 0;
         mem_write_out <= 0;
         mem_read_out <= 0;
         mem_to_reg_out <= 0;
         branch_out <= 0;
     end
     else if(!stall) begin
-        pc_plus4_out <= pc_plus4_in;
-        rd1_out <=  rd1_in;
+        alu_result_out <= alu_result_in;
         rd2_out <=  rd2_in;
-        imm_out <=  imm_in;
-        rs1_out <= rs1_in; 
-        rs2_out <= rs2_in; 
         rd_out <= rd_in; 
-        alu_op_out <= alu_op_in;
+        pc_branch_out <= pc_branch_in;
         reg_write_out <= reg_write_in;
-        alu_src_out <= alu_src_in;
+        zero_out <= zero_in;
         mem_write_out <= mem_write_in;
         mem_read_out <= mem_read_in;
         mem_to_reg_out <= mem_to_reg_in;
@@ -65,5 +56,4 @@ always @(posedge clk)begin
        
     end
 end
-
 endmodule
