30b56da50348cef60ca922f3475d067671742499
akulasahasra75 <akulasahasra0705@gmail.com>
2026-06-07 11:36:42 +0530

    add data_mem, synchronous write combinatorial read

---
+++ src/data_mem.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.06.2026 11:25:40
+// Design Name: 
+// Module Name: data_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_mem(
+
+    );
+endmodule
